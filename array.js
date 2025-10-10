2269ee2ac18333b63643ea46c7d9fca47ffda019
Tisha <sumaiyataslim43@gmail.com>
2025-10-10 21:18:36 +0600

    updated array file

---
+++ array.js
@@ -0,0 +1,19 @@
+// // Array
+// const numbers = [1, 2, 3, 4, 5];
+// const reversed = [];
+// numbers.reverse();
+// console.log(numbers);
+
+// // task-1
+
+// const colors = ["red", "blue", "green", "yellow", "orange"];
+// const reversed = [];
+// colors.reversed();
+
+// // task-2
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 == 0) {
+//     console.log(numbers[i]);
+//   }
+// }
