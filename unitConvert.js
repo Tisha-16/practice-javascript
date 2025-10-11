4300bd18a7f8dc43422d2bde8e84f23148331b9a
Tisha <sumaiyataslim43@gmail.com>
2025-10-11 23:34:41 +0600

    doing some conversion

---
+++ unitConvert.js
@@ -0,0 +1,16 @@
+// // 12 inch 1 feet
+// function inchToFeet(inch) {
+//   const feet = inch / 12;
+//   return feet;
+// }
+// const height = inchToFeet(75);
+// console.log(height);
+
+// // kilo to mile
+
+// function mileTokilo(mile) {
+//   const kilo = mile * 1.60934;
+//   return kilo;
+// }
+// const km = mileTokilo(30);
+// console.log(km);
