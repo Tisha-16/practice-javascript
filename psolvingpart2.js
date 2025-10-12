bcfabdcbc2a1910057f5e894b106a515049ac032
Tisha <sumaiyataslim43@gmail.com>
2025-10-12 21:15:50 +0600

    simple calculator

---
+++ psolvingpart2.js
@@ -0,0 +1,17 @@
+// // who will get the strawberry
+// const max = Math.max(32, 25);
+// console.log("j boro she strawberry pabe:", max);
+
+// // find cheapest phone using array
+// const prices = [20000, 16000, 50000, 12000];
+// function getMin(numbers) {
+//   let min = numbers[0];
+//   for (const num of numbers) {
+//     if (num < min) {
+//       min = num;
+//     }
+//   }
+//   return min;
+// }
+// const cheap = getMin(prices);
+// console.log("cheapest one is:", cheap);
