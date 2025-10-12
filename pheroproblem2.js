55b915dd6f8109894a38cdc12bee89f7a4bad115
Tisha <sumaiyataslim43@gmail.com>
2025-10-12 23:25:36 +0600

    solving problems

---
+++ pheroproblem2.js
@@ -0,0 +1,42 @@
+// // Problem-01 : Train TT's Fine Calculator
+// function totalFine(fare) {}
+
+// // Problem-02 : Clean & Capitalize Characters
+// function onlyCharacter(str) {
+//   if (typeof str !== "string") {
+//     return Invalid;
+//   }
+//   const trim = str.replace(/\s+/g, "");
+//   const upper = trim.toUpperCase();
+//   return upper;
+// }
+// console.log(onlyCharacter("  h e llo wor   ld"));
+
+// // Problem-03 : FIFA Best Team Award
+// const player1 = {
+//   name: "Brazil",
+//   foul: 5,
+//   cardY: 1,
+//   cardR: 0,
+// };
+// const player2 = {
+//   name: "Argentina",
+//   foul: 7,
+//   cardY: 0,
+//   cardR: 0,
+// };
+// function bestTeam(player1, player2) {
+//   const total1 = player1.foul + player1.cardY + player1.cardR;
+//   const total2 = player2.foul + player2.cardY + player2.cardR;
+
+//   if (total1 > total2) {
+//     return player2.name;
+//   } else if (total1 < total2) {
+//     return player1.name;
+//   } else {
+//     return Tie;
+//   }
+// }
+
+// const obj = bestTeam(player1, player2);
+// console.log(obj);
